9fc4c4731661242952eb43f36d6cabc652f95fc2
elliot richardson <ear78@aol.com>
2017-09-20 00:18:44 -0500

    first commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>CI Blog</title>
+        <link rel="stylesheet" href="https://bootswatch.com/darkly/bootstrap.min.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="<?php echo base_url(); ?>">CI Blog</a>
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="<?php echo base_url(); ?>">Home</a></li>
+                <li><a href="<? echo base_url(); ?>about">About</a></li>
+                <li><a href="<?php echo base_url();?>contact">Contact</a></li>
+              </ul>
+            </div><!--/.nav-collapse -->
+          </div>
+      </nav>
+
+      <div class="container">
